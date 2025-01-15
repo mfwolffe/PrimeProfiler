777a038e73ba612357dcd31aa709262574f80343
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-15 03:35:12 -0500

    put prototype for test in header

--- src/primes.h
+++ src/primes.h
@@ -9,8 +9,28 @@ typedef struct {
   unsigned int n;
 } case_t;
 
+
+void test_candidates(case_t *cndts, int n_cndts, const char* algo_str, bool (*fn_to_test)(unsigned int));
+
+/* algorithms to instrument */
+
+/*
+ * brute-force, no sqrt shortcut
+ *
+ */
 bool naive_prime(unsigned int n);
+
+
+/*
+ * brute-force, but with sqrt shortcut
+ *
+ */
 bool naive_prime_squares(unsigned int n);
+
+/*
+ * 6k +/- 1 + sqrt shortcut
+ *
+ */
 bool less_naive_prime(unsigned int n);
 
 #endif // PRIMES_H
